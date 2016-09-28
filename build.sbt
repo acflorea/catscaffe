cfa2ff37cdc7176b04b3d642a5591e5cc29ef9b1
Adrian Florea <acflorea@gmail.com>
2016-09-28 18:42:20 +0300

    More fun with the cats

--- build.sbt
+++ build.sbt
@@ -4,14 +4,20 @@ version := "2.4.4"
 
 scalaVersion := "2.11.7"
 
+val akkaVersion = "2.4.4"
+
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.4.4",
-  "com.typesafe.akka" %% "akka-persistence" % "2.4.4",
-  "org.iq80.leveldb"            % "leveldb"          % "0.7",
-  "org.fusesource.leveldbjni"   % "leveldbjni-all"   % "1.8"
+  "org.slf4j" % "slf4j-api" % "1.7.7",
+  "ch.qos.logback" % "logback-core" % "1.1.2",
+  "ch.qos.logback" % "logback-classic" % "1.1.2",
+
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
+  "org.iq80.leveldb" % "leveldb" % "0.7",
+  "org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8",
+  "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
 )
 
 licenses := Seq(("CC0", url("http://creativecommons.org/publicdomain/zero/1.0")))
-
 
 fork in run := true
--- src/main/scala/catscaffe/test/CatsHerding.scala
+++ src/main/scala/catscaffe/test/CatsHerding.scala
@@ -1,14 +1,19 @@
 package catscaffe.test
 
 import akka.actor.{ActorSystem, Props}
-import catscaffe.domain.CatAggregate.Play
 import catscaffe.service.CatsAggregateManager
-import catscaffe.service.CatsAggregateManager.{AddCatToHerd, FeedCat, PlayWithCat}
+import catscaffe.service.CatsAggregateManager.{AddCatToHerd, FeedCat, GetCat, PlayWithCat}
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by aflorea on 28.09.2016.
  */
 object CatsHerding extends App {
+
+  implicit val timeout = Timeout(5 seconds) // needed for `?` below
 
   val system = ActorSystem("catcaffe")
   val herdManager = system.actorOf(Props[CatsAggregateManager], "cats-herd-manager")
@@ -17,19 +22,19 @@ object CatsHerding extends App {
   // val subscriberActor1 = system.actorOf(Props[ExampleSubscriber], "subscriberActor-1-scala")
   // val subscriberActor2 = system.actorOf(Props[ExampleSubscriber], "subscriberActor-2-scala")
 
-  herdManager ! AddCatToHerd("Fely", 1)
-  herdManager ! AddCatToHerd("Bri", 1)
-  herdManager ! AddCatToHerd("Barek", 1)
+//  herdManager ? AddCatToHerd("Fely", 1) map println
+//  herdManager ? AddCatToHerd("Bri", 1) map println
+//  herdManager ? AddCatToHerd("Barek", 1) map println
 
-  herdManager ! PlayWithCat("Fely", 10)
-  herdManager ! PlayWithCat("Fely", 5)
-  herdManager ! PlayWithCat("Fely", 5)
-  herdManager ! PlayWithCat("Fely", 5)
+//  herdManager ? PlayWithCat("Fely", 10) map println
+  herdManager ? PlayWithCat("Fely", 5) map println
+//  herdManager ? PlayWithCat("Bri", 5) map println
+//  herdManager ? PlayWithCat("Fely", 5) map println
 
-  herdManager ! FeedCat("Fely", 10)
-  herdManager ! FeedCat("Fely", 5)
-  herdManager ! FeedCat("Fely", 5)
-
+//  herdManager ? FeedCat("Fely", 10) map println
+//  herdManager ? FeedCat("Fely", 5) map println
+//  herdManager ? FeedCat("Barek", 5) map println
+//  herdManager ? FeedCat("Bri", 5) map println
 
   Thread.sleep(1000)
   system.terminate()
